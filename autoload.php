e797d3a2aef7e3bb563ccab9351a1e6506bdf535
Artem Zaytsev <arepo@nologin.ru>
2015-10-17 20:36:37 +0300

    Добавил автолоад

--- autoload.php
+++ autoload.php
@@ -15,5 +15,3 @@ spl_autoload_register(function ($class) {
 	if (file_exists($file))
 		require_once $file;
 });
-
-require(__DIR__ . '/src/D2.php');
--- src/D2.php
+++ src/D2.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace d2;
 
 class D2
@@ -34,18 +33,3 @@ class D2
 		return $insert;
 	}
 }
-
-//
-//$quote = new Quote;
-//$d2 = new D2;
-//$insert = new Insert('sss.aaa');
-//$insert->ignore();
-//$insert->row(['a' => new BiOperation(new Identifier('field'), '=', new Constant(1000))]);
-//$insert->onDuplicateKeyUpdate();
-//
-//echo $insert->toString($quote), "\n";
-//
-//$where = new Where();
-//$where->addAnd(new BiOperation(new Identifier('column'), '=', new Constant('column')));
-//$where->addAnd(new NotIn(new Identifier('column'), [new Constant('v0'), new Constant('v1')]));
-//echo $where->toString($quote), "\n";
