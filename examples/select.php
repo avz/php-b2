c0e9057a521bf6e08b0d30ca47bfdeb1842562d0
Artem Zaytsev <arepo@nologin.ru>
2015-10-20 20:43:44 +0300

    Дополнил пример JOIN'ом

--- examples/select.php
+++ examples/select.php
@@ -6,4 +6,15 @@ $quote = new b2\tests\mock\Quote();
 
 $b2 = new b2\B2;
 
-echo $b2->select('user', 'id > ?', [10])->column('uid')->toString($quote), "\n";
+echo $b2->select('user', 'id > ?', [10])
+	->column('uid')
+	->column('name')
+	->column('xp')
+	->toString($quote)
+, "\n";
+
+echo $b2->select('user', 'id = ?', [10])
+	->leftJoin('payment', 'payment.id = user.id')
+	->column($b2->sql('SUM(payment.price * ?)', [10]), 'sum')
+	->toString($quote)
+, "\n";
