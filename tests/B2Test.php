d56bd20f467ec56fee8e4e7192823198141a0722
Artem Zaytsev <arepo@nologin.ru>
2015-10-20 12:01:56 +0300

    Добавил простеньких тестов на delete, update и insert

--- tests/B2Test.php
+++ tests/B2Test.php
@@ -3,7 +3,7 @@ namespace b2\tests;
 
 class B2Test extends \b2\tests\Base
 {
-	public function testSelectWhere() {
+	public function testSelect() {
 		$b2 = new \b2\B2;
 
 		$expected = new \b2\query\Select('user');
@@ -30,6 +30,54 @@ class B2Test extends \b2\tests\Base
 
 		$actual = $b2->select('user', ['a' => 'b']);
 
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testUpdate() {
+		$b2 = new \b2\B2;
+
+		$expected = new \b2\query\Update('user');
+		$expected->where(['a' => 'b']);
+		$actual = $b2->update('user', ['a' => 'b']);
+		$this->assertEquals($expected, $actual);
+
+		$expected = new \b2\query\Update('user');
+		$expected->where('hello = "world"');
+		$actual = $b2->update('user', 'hello = "world"');
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testDelete() {
+		$b2 = new \b2\B2;
+
+		$expected = new \b2\query\Delete('user');
+		$expected->where(['a' => 'b']);
+		$actual = $b2->delete('user', ['a' => 'b']);
+		$this->assertEquals($expected, $actual);
+
+		$expected = new \b2\query\Delete('user');
+		$expected->where('hello = "world"');
+		$actual = $b2->delete('user', 'hello = "world"');
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testInsert() {
+		$b2 = new \b2\B2;
+
+		$expected = new \b2\query\Insert('user');
+		$actual = $b2->insert('user');
+		$this->assertEquals($expected, $actual);
+
+		$expected = new \b2\query\Insert('user');
+		$expected->row(['a' => 'b']);
+
+		$actual = $b2->insert('user', [['a' => 'b']]);
+		$this->assertEquals($expected, $actual);
+
+		$expected = new \b2\query\Insert();
+		$expected->table('user');
+		$expected->rows([['a' => 'b'], ['a' => 'd']]);
+		$actual = $b2->insert('user', [['a' => 'b'], ['a' => 'd']]);
 		$this->assertEquals($expected, $actual);
 	}
 }
